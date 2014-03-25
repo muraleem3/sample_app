bb852cdcd3df7f3ccaa6730a1896fe31f4a98764
Muralee Mohan <muraleem3@gmail.com>
2014-03-25 09:51:35 +0530

    heroku display

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ SampleApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+
+  root to: 'static_pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -22,7 +25,7 @@ SampleApp::Application.routes.draw do
   #     member do
   #       get 'short'
   #       post 'toggle'
-  #     end
+  #     end 
   #
   #     collection do
   #       get 'sold'
